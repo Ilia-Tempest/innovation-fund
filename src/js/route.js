86219b561391e149eef486b61051cc5fdcddb1fa
unknown <iparshacov@gmail.com>
2021-08-06 13:55:55 +0500

    Закончил главную страницу

---
+++ src/js/route.js
@@ -0,0 +1,15 @@
+$('.route-item').click(function(){
+		
+        $('.route-min').toggleClass('route-line');
+      
+      
+        
+	});
+
+$('.route-item-1').click(function(){
+		
+        $('.route-min-1').toggleClass('route-line');
+      
+      
+        
+	});
